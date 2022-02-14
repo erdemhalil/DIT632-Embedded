091794e77d4a388f2011e34496d6962891e3e3ba
erdemhalil <erdem-2001@windowslive.com>
2022-02-15 00:21:58 +0100

    Add exercise 5

---
+++ work_package_3/exercise5.ino
@@ -0,0 +1,40 @@
+// header file for Keypad library
+#include <Keypad.h>
+
+unsigned char rows = 4; // initiate number of rows on the keypad
+unsigned char cols = 4; // initiate number of columns on the keypad
+
+unsigned char maxColspins[4] = {2, 3, 4, 5}; // pin numbers on the column
+unsigned char maxRowspins[4] = {8, 9, 10, 11}; // pin numbers on the row
+
+char values[4][4] = {  
+    {'1', '2', '3', 'A'},
+    {'4', '5', '6', 'B'},  // matrix for the buttons to keep them in a particular order
+    {'7', '8', '9', 'C'},  
+    {'*', '0', '#', 'D'}};
+
+
+// Initiate the keypad 
+Keypad keypad = Keypad(makeKeymap(values), maxRowspins, maxColspins, rows, cols);
+
+void setup()
+{
+  // Start the serial port
+  Serial.begin(9600);
+}
+
+
+void loop()
+{
+  // Get the current pressed key from the keypad
+  char pressedKey = keypad.getKey();
+  // Check if any key is pressed
+  if (pressedKey)
+  {
+    // Delay the output
+    delay(1000);
+    // Print the pressed key to the serial
+    Serial.print("Keypad pressed: ");
+    Serial.println(pressedKey);
+  }
+}
